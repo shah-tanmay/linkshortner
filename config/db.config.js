29ed6dde67d582200927801b9687d1d8830ba80c
Tanmay Shah <shahhtanmay@gmail.com>
2022-07-26 22:02:43 +0530

    fixes

--- config/db.config.js
+++ config/db.config.js
@@ -1,12 +1,10 @@
-const mongoose = require('mongoose')
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_URI = process.env.DB_URI;
 
-const DB_URI = `mongodb+srv://STClinkShortner:${DB_PASSWORD}@cluster0.fewup4x.mongodb.net/test`
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect(DB_URI,{useNewUrlParser:true, useUnifiedTopology:true})
+const connection = mongoose.connection;
 
-const connection = mongoose.connection
-
-module.exports = connection
+module.exports = connection;
